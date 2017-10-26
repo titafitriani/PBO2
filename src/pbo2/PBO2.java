75ea944af1638fc6bdbdbd422415066bf51cef6e
Tita Fitriani <33127868+titafitriani@users.noreply.github.com>
2017-10-27 02:08:24 +0800

    Initial commit

---
+++ src/pbo2/PBO2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2;
+
+/**
+ *
+ * @author proWindows 10
+ */
+public class PBO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
