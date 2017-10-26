b9a84c726d8a7afe993657c84bf967f03c80048a
Tita Fitriani <33127868+titafitriani@users.noreply.github.com>
2017-10-27 02:20:00 +0800

    mengisi attribute dari object "m

--- src/pbo2/PBO2.java
+++ src/pbo2/PBO2.java
@@ -3,6 +3,9 @@ public class PBO2 {
 
     public static void main(String[] args) {
         myFrame m = new myFrame();
+        m.pesan="Halloo...";
+        m.bilBulat=5;
+        m.bilDesimal=3.14;
     }
     
 }
